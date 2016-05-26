9a9071cc12b4a2e5822405c5678ca003645371e9
raviguru123 <raviguruiitr@gmail.com>
2016-05-26 10:35:27 +0530

    first angular directory establish

--- js/index.js
+++ js/index.js
@@ -1,8 +1,35 @@
-var app=angular.module("index.module",[]);
+var app=angular.module("directive.module",[]);
 
-app.controller("indexController",indexController);
+app.controller("DirectiveController",DirectiveController);
 
-function indexController($scope){
+function DirectiveController($scope){
 
 	$scope.pageName="index.html";
+	$scope.outervalue="outervalueSuccess";
+	$scope.check=function(){
+		alert("check function call");
+	}
 }
+
+
+
+app.directive('cardLen', function () {
+  return {
+    restrict: 'E',
+    scope: true,
+   /* template:['<div style="width:100px;height: 100px;background-color: green">',
+'<label>Directive</label>',
+	'<ul>',
+		'<li>hello</li>',
+		'<li ng-click="check()">second</li>',
+		'<li>second</li>',
+	'</ul>',
+'</div'].join(''),*/
+	templateUrl:'./directory/dir1.html',
+    link: function (scope,elements,attr,controller,transclude) {
+      //scope.outerVal = "inner";
+      //scope.model.innerVal = "inner2";
+      //scope.innerModel = {innerVal: "innerVal"};
+    }
+  }
+});
